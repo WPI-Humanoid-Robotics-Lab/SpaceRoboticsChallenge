593caf24f81c8e9ac70f63e67717dd49b1266f08
sumanth <sumanth.724@gmail.com>
2016-11-04 03:24:26 -0400

    added self collision check commneted code for refacturing

--- pose_pkg/src/Reachability_Matrix.cpp
+++ pose_pkg/src/Reachability_Matrix.cpp
@@ -1,9 +1,8 @@
 #include <ros/ros.h>
-
-// MoveIt!
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
 #include <moveit/robot_state/robot_state.h>
+#include <moveit/planning_scene/planning_scene.h>
 
 int main(int argc, char **argv)
 {
@@ -11,36 +10,36 @@ int main(int argc, char **argv)
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
-    /*
-  // We will start by instantiating a
-  // `RobotModelLoader`_
-  // object, which will look up
-  // the robot description on the ROS parameter server and construct a
-  // :moveit_core:`RobotModel` for us to use.
-  //
-  // .. _RobotModelLoader: http://docs.ros.org/indigo/api/moveit_ros_planning/html/classrobot__model__loader_1_1RobotModelLoader.html
-  */
+    // instatinate robot model and get the description
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
     robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+
     ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
-    /*
-  // Using the :moveit_core:`RobotModel`, we can construct a
-  // :moveit_core:`RobotState` that maintains the configuration
-  // of the robot. We will set all joints in the state to their
-  // default values. We can then get a
-  // :moveit_core:`JointModelGroup`, which represents the robot
-  // model for a particular group, e.g. the "right_arm" of the PR2
-  // robot.
-*/
+
+    // construct the robot state
     robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
+    // set all joints to default state
     kinematic_state->setToDefaultValues();
+
+    // instantiate planning scene
+    planning_scene::PlanningScene planning_scene(kinematic_model);
+
+    // create collision variables for self collision
+    collision_detection::CollisionRequest collision_request;
+    collision_detection::CollisionResult collision_result;
+
+    // get the move group
     const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("left_palm");
+
+    // get the joints in the move group
     const std::vector<std::string> &joint_names = joint_model_group->getJointModelNames();
+
+    // set collision request for the move group
+    collision_request.group_name = "left_palm";
+
 
 
     //moveit::core::JointBoundsVector obj=joint_model_group->getActiveJointModelsBounds();
-
-
     // Get Joint Values
     // ^^^^^^^^^^^^^^^^
     // We can retreive the current set of joint values stored in the state for the left palm.
@@ -125,12 +124,13 @@ int main(int argc, char **argv)
     ros::NodeHandle n1;
     ros::Publisher vis_pub = n1.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
 
+    //visualization_msgs::MarkerArray markerArray;
     visualization_msgs::Marker marker;
     marker.header.frame_id = "pelvis";
     marker.header.stamp = ros::Time();
     marker.ns = "my_namespace";
     marker.id = 0;
-    marker.type = visualization_msgs::Marker::CUBE;
+    marker.type = visualization_msgs::Marker::CUBE_LIST;
     marker.action = visualization_msgs::Marker::ADD;
     marker.pose.position.x = 0;
     marker.pose.position.y = 0;
@@ -139,9 +139,9 @@ int main(int argc, char **argv)
     marker.pose.orientation.y = 0.0;
     marker.pose.orientation.z = 0.0;
     marker.pose.orientation.w = 1.0;
-    marker.scale.x = 0.1;
-    marker.scale.y = 0.1;
-    marker.scale.z = 0.1;
+    marker.scale.x = 0.03;
+    marker.scale.y = 0.03;
+    marker.scale.z = 0.03;
     marker.color.a = 1.0; // Don't forget to set the alpha!
     marker.color.r = 0.0;
     marker.color.g = 1.0;
@@ -168,10 +168,8 @@ int main(int argc, char **argv)
                                 {
                                     for(float j9 = ll_9; j9 < ul_9; j9=j9+sample_dt)
                                     {
-                                        for(float j10 = ll_10; j10 < ul_10;j10=j10+sample_dt )
+                                        for(float j10 = ll_10; j10 < ul_10;j10=j10+sample_dt)
                                         {
-                                            //   ROS_INFO("iNNER Loop reached");
-
                                             joint_values[0] = j1;
                                             joint_values[1] = j2;
                                             joint_values[2] = j3;
@@ -182,23 +180,30 @@ int main(int argc, char **argv)
                                             joint_values[7] = j8;
                                             joint_values[8] = j9;
                                             joint_values[9] = j10;
-                                            //ROS_INFO("sET JOint angles");
+
                                             kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
-                                            //kinematic_state->setToRandomPositions(joint_model_group);
-                                            const Eigen::Affine3d &end_effector_state = kinematic_state->getGlobalLinkTransform("leftPalm");
 
-                                            // Print end-effector pose. Remember that this is in the model frame
-                                            ROS_INFO_STREAM("Translation: " << end_effector_state.translation());
-                                            marker.pose.position.x = end_effector_state.translation()[0];
-                                            marker.pose.position.y = end_effector_state.translation()[1];
-                                            marker.pose.position.z = end_effector_state.translation()[2];
+                                            // check the slef collision
+                                            collision_result.clear();
+                                            planning_scene.checkSelfCollision(collision_request, collision_result);
 
-                                            //ROS_INFO("Translation in x-axis %f", marker.pose.position.x);
+                                            // if there is no self collision
+                                            if (collision_result.collision != true)
+                                            {
+                                                // get the transform and update the rechability map
+                                                const Eigen::Affine3d &end_effector_state = kinematic_state->getGlobalLinkTransform("leftPalm");
 
-                                            vis_pub.publish( marker );
+                                                geometry_msgs::Point p;
+                                                // Print end-effector pose. Remember that this is in the model frame
+                                                ROS_INFO_STREAM("Translation: " << end_effector_state.translation());
 
 
+                                                p.x = end_effector_state.translation()[0];
+                                                p.y = end_effector_state.translation()[1];
+                                                p.z = end_effector_state.translation()[2];
 
+                                                marker.points.push_back(p);
+                                            }
                                         }
                                     }
                                 }
@@ -206,23 +211,33 @@ int main(int argc, char **argv)
                         }
                     }
                 }
-
             }
-
         }
     }
 
-
-    /*
-  kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
-  //kinematic_state->setToRandomPositions(joint_model_group);
-  const Eigen::Affine3d &end_effector_state = kinematic_state->getGlobalLinkTransform("leftPalm");
-
-  // Print end-effector pose. Remember that this is in the model frame
-  ROS_INFO_STREAM("Translation: " << end_effector_state.translation());
-  ROS_INFO_STREAM("Rotation: " << end_effector_state.rotation());
-  ros::shutdown();
-  */
+    // publish the markers
+    vis_pub.publish(marker);
 
     return 0;
 }
+
+
+//{
+
+//// get the  move group
+//const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("right_arm");
+//// get the joint model names
+//const std::vector<std::string> &joint_names = joint_model_group->getJointModelNames();
+//// get the number of joints
+//std::size_t jointNumber = joint_names.size();
+
+//// get the joint limits
+//robot_model::JointBoundsVector joint_bounds;
+//joint_bounds[0].
+//joint_bounds = joint_model_group->getActiveJointModelsBounds();
+
+//// sample through all the joints and generate the recheability map
+//while (i != jointNumber)
+//{
+// for (i=)
+//}
