3b9c99ffa299a718c79c2af5a41d6c1ef4f1060c
sumanth <sumanth.724@gmail.com>
2016-11-07 01:52:01 -0500

    added marker for collision points added yaml inputs for collision points

--- val_rechability/src/val_rechability/val_rechability.cpp
+++ val_rechability/src/val_rechability/val_rechability.cpp
@@ -10,6 +10,8 @@
 #include "H5Cpp.h"
 #include <hdf5.h>
 
+void print_status(long current_count, double total);
+
 int main(int argc, char **argv)
 {
     ros::init (argc, argv, "Reachability_Matrix");
@@ -108,10 +110,11 @@ int main(int argc, char **argv)
     //        }
     //    }
 
-    float sample_dt = 0.2;
+    float sample_dt = 0.4;
 
     ros::NodeHandle n1;
     ros::Publisher vis_pub = n1.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
+    ros::Publisher vis_pub_no = n1.advertise<visualization_msgs::Marker>( "visualization_marker_no", 0 );
 
     //visualization_msgs::MarkerArray markerArray;
     visualization_msgs::Marker marker;
@@ -133,9 +136,32 @@ int main(int argc, char **argv)
     marker.scale.z = 0.03;
     marker.color.a = 1.0; // Don't forget to set the alpha!
     marker.color.r = 0.0;
-    marker.color.g = 1.0;
+    marker.color.g = 0.7;
     marker.color.b = 0.0;
     marker.lifetime = ros::Duration(0);
+
+    visualization_msgs::Marker marker_no;
+    marker_no.header.frame_id = "pelvis";
+    marker_no.header.stamp = ros::Time();
+    marker_no.ns = "my_namespace_no";
+    marker_no.id = 0;
+    marker_no.type = visualization_msgs::Marker::CUBE_LIST;
+    marker_no.action = visualization_msgs::Marker::ADD;
+    marker_no.pose.position.x = 0;
+    marker_no.pose.position.y = 0;
+    marker_no.pose.position.z = 0;
+    marker_no.pose.orientation.x = 0.0;
+    marker_no.pose.orientation.y = 0.0;
+    marker_no.pose.orientation.z = 0.0;
+    marker_no.pose.orientation.w = 1.0;
+    marker_no.scale.x = 0.03;
+    marker_no.scale.y = 0.03;
+    marker_no.scale.z = 0.03;
+    marker_no.color.a = 1.0; // Don't forget to set the alpha!
+    marker_no.color.r = 0.7;
+    marker_no.color.g = 0.0;
+    marker_no.color.b = 0.1;
+    marker_no.lifetime = ros::Duration(0);
 
     char the_path[256];
 
@@ -176,6 +202,13 @@ int main(int argc, char **argv)
 
     FILE* yaml = fopen(fullpath.c_str(), "w");
 
+    double total=1;
+    long z=0;
+    for(int i=0;i<jointCount;i++)
+    {
+        total*=((((*joint_bounds[i])[0].max_position_)-((*joint_bounds[i])[0].min_position_))/sample_dt);
+    }
+
     for(float j1 = (*joint_bounds[0])[0].min_position_; j1 < (*joint_bounds[0])[0].max_position_; j1=j1+sample_dt)
     {
         for(float j2 = (*joint_bounds[1])[0].min_position_; j2 < (*joint_bounds[1])[0].max_position_; j2=j2+sample_dt)
@@ -196,18 +229,6 @@ int main(int argc, char **argv)
                                     {
                                         for(float j10 = (*joint_bounds[9])[0].min_position_; j10 < (*joint_bounds[9])[0].max_position_;j10=j10+sample_dt)
                                         {
-
-                                            //                                            joint_values[0] = 0; //j1;
-                                            //                                            joint_values[1] = 0; // -0.13; //j2;
-                                            //                                            joint_values[2] = 0.12; //j3;
-                                            //                                            joint_values[3] = 0.43; //j4;
-                                            //                                            joint_values[4] = -0.86; //j5;
-                                            //                                            joint_values[5] = 0; //j6;
-                                            //                                            joint_values[6] = 0; //j7;
-                                            //                                            joint_values[7] = 0; //j8;
-                                            //                                            joint_values[8] = 0; //j9;
-                                            //                                            joint_values[9] = 0; //j10;
-
                                             joint_values[0] = j1;
                                             joint_values[1] = j2;
                                             joint_values[2] = j3;
@@ -219,6 +240,9 @@ int main(int argc, char **argv)
                                             joint_values[8] = j9;
                                             joint_values[9] = j10;
 
+                                            //print_status(z, total);
+                                            z++;
+
                                             // kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
                                             // kinematic_state->update(true);
                                             // kinematic_state->updateCollisionBodyTransforms();
@@ -226,51 +250,55 @@ int main(int argc, char **argv)
                                             current_state = planning_scene.getCurrentStateNonConst();
                                             current_state.setJointGroupPositions(joint_model_group, joint_values);
                                             // current_state.setToRandomPositions();
-                                            ROS_INFO_STREAM("Current state is "<< (current_state.satisfiesBounds(joint_model_group) ? "valid" : "not valid"));
+                                            // ROS_INFO_STREAM("Current state is "<< (current_state.satisfiesBounds(joint_model_group) ? "valid" : "not valid"));
                                             collision_request.contacts = true;
                                             collision_request.max_contacts = 1000;
 
                                             // check the slef collision
                                             collision_result.clear();
                                             planning_scene.checkSelfCollision(collision_request, collision_result);
-                                            ROS_INFO_STREAM("Current state is "<< (collision_result.collision ? "in" : "not in")
-                                                            << " self collision");
+                                            //  ROS_INFO_STREAM("Current state is "<< (collision_result.collision ? "in" : "not in")
+                                            // << " self collision");
                                             collision_detection::CollisionResult::ContactMap::const_iterator it;
                                             for(it = collision_result.contacts.begin();
                                                 it != collision_result.contacts.end();
                                                 ++it)
                                             {
-                                                ROS_INFO("Contact between: %s and %s",it->first.first.c_str(),it->first.second.c_str());
+                                                // ROS_INFO("Contact between: %s and %s",it->first.first.c_str(),it->first.second.c_str());
                                             }
 
                                             std::vector<double> joint_values;
                                             current_state.copyJointGroupPositions(joint_model_group, joint_values);
                                             for(std::size_t i = 0; i < jointCount; ++i)
                                             {
-                                                ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+                                                //  ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
                                             }
 
                                             // if there is no self collision
+                                            //                                            if (collision_result.collision != true)
+                                            //                                            {
+                                            // get the transform and update the rechability map
+                                            const Eigen::Affine3d &end_effector_state = current_state.getGlobalLinkTransform("leftPalm"); // kinematic_state->getGlobalLinkTransform("leftPalm");
+
+                                            geometry_msgs::Point p;
+                                            // Print end-effector pose. Remember that this is in the model frame
+                                            // ROS_INFO_STREAM("Translation: " << end_effector_state.translation());
+                                            //   ROS_INFO("Valid State");
+
+                                            p.x = end_effector_state.translation()[0];
+                                            p.y = end_effector_state.translation()[1];
+                                            p.z = end_effector_state.translation()[2];
+
                                             if (collision_result.collision != true)
                                             {
-                                                // get the transform and update the rechability map
-                                                const Eigen::Affine3d &end_effector_state = kinematic_state->getGlobalLinkTransform("leftPalm");
-
-                                                geometry_msgs::Point p;
-                                                // Print end-effector pose. Remember that this is in the model frame
-                                                ROS_INFO_STREAM("Translation: " << end_effector_state.translation());
-                                                ROS_INFO("Valid State");
-
-                                                p.x = end_effector_state.translation()[0];
-                                                p.y = end_effector_state.translation()[1];
-                                                p.z = end_effector_state.translation()[2];
-
-                                                fprintf(yaml, "map: %s\nresolution: %f\npoints: [%f, %f, %f]\n\n", filename.c_str(), sample_dt, p.x, p.y, p.z);
+                                                fprintf(yaml, "map: %s\nresolution: %f\nself collision: %s\npoints: [%f, %f, %f]\n\n", filename.c_str(), sample_dt, "false", p.x, p.y, p.z);
                                                 marker.points.push_back(p);
                                             }
                                             else
                                             {
-                                                ROS_INFO("collison");
+                                                fprintf(yaml, "map: %s\nresolution: %f\nself collision: %s\npoints: [%f, %f, %f]\n\n", filename.c_str(), sample_dt, "true", p.x, p.y, p.z);
+                                                marker_no.points.push_back(p);
+                                                //      ROS_WARN("collison");
                                             }
                                         }
                                     }
@@ -283,10 +311,15 @@ int main(int argc, char **argv)
         }
     }
 
+    // close the files
     fclose(yaml);
+    H5Fclose(file);
+    H5Gclose(group_poses);
+
     ROS_INFO("Done\n");
     // publish the markers
     vis_pub.publish(marker);
+    vis_pub_no.publish(marker_no);
 
     return 0;
 }
@@ -301,3 +334,23 @@ int main(int argc, char **argv)
 //    }
 //}
 
+
+
+
+
+void print_status(long current_count, double total)
+{
+    double Y=(current_count/total)*100;
+    //ROS_INFO("%ld %lf",current_count, total);
+
+    if (Y>0 && Y<10) std::cout << "Completed    " << Y << " %-" << std::endl;
+    if (Y>10 && Y<20) std::cout << "Completed   "<<Y<< " %--" << std::endl;
+    if (Y>20 && Y<30) std::cout << "Completed   "<<Y<< " %---" << std::endl;
+    if (Y>30 && Y<40) std::cout << "Completed   "<<Y<< " %----" << std::endl;
+    if (Y>40 && Y<50) std::cout << "Completed   "<<Y<< " %-----" << std::endl;
+    if (Y>50 && Y<60) std::cout << "Completed   "<<Y<< " %------" << std::endl;
+    if (Y>60 && Y<70) std::cout << "Completed   "<<Y<< " %--------" << std::endl;
+    if (Y>70 && Y<80) std::cout << "Completed   "<<Y<< " %---------" << std::endl;
+    if (Y>80 && Y<90) std::cout << "Completed   "<<Y<< " %----------" << std::endl;
+    if (Y>90) std::cout << "Completed  "<<Y<< " %----------" << std::endl;
+}
