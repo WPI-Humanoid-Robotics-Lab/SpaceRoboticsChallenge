920ae0a71096865af03b38197ea56857ad396a5e
muddysahil <skejriwal@wpi.edu>
2017-04-12 14:13:34 -0400

    Dual arm Manipulation - tested

--- val_motion_planners/val_moveit/val_moveit_tasks/src/dual_arm_moveit.cpp
+++ val_motion_planners/val_moveit/val_moveit_tasks/src/dual_arm_moveit.cpp
@@ -19,27 +19,38 @@ int main(int argc, char** argv){
      *************************************/
   ros::AsyncSpinner spinner(1);
   spinner.start();
-  moveit::planning_interface::MoveGroup group("botharms");
+  moveit::planning_interface::MoveGroup groupRight("rightShoulderPalm");
+  moveit::planning_interface::MoveGroup groupLeft("leftShoulderPalm");
 
     ROS_INFO("set to state");
   // set the start state to the current state of the robot
-  group.setStartStateToCurrentState();
+  groupRight.setStartStateToCurrentState();
+  groupLeft.setStartStateToCurrentState();
 
   // get the cuurent joints and their positions
-  std::vector<std::string> jNames;
-  jNames = group.getActiveJoints();
-  std::vector<double> jValues;
-  jValues = group.getCurrentJointValues();
+  std::vector<std::string> jNamesRight,jNamesLeft;
+  jNamesRight = groupRight.getActiveJoints();
+  jNamesLeft = groupLeft.getActiveJoints();
+  std::vector<double> jValuesRight, jValuesLeft;
+  jValuesRight = groupRight.getCurrentJointValues();
+  jValuesLeft = groupLeft.getCurrentJointValues();
   std::vector<std::string>::iterator it;
   std::vector<double>::iterator itd;
   ROS_INFO("current state");
-  for (it=jNames.begin(), itd=jValues.begin(); it<jNames.end(); it++, itd++){
+  for (it=jNamesRight.begin(), itd=jValuesRight.begin(); it<jNamesRight.end(); it++, itd++){
     std::cout << *it << ": " << *itd << std::endl;
   }
 
-  ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
-  ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
 
+  for (it=jNamesLeft.begin(), itd=jValuesLeft.begin(); it<jNamesLeft.end(); it++, itd++){
+    std::cout << *it << ": " << *itd << std::endl;
+  }
+
+  ROS_INFO("Reference frame for Right: %s", groupRight.getPlanningFrame().c_str());
+  ROS_INFO("Reference frame for right end effector: %s", groupRight.getEndEffectorLink().c_str());
+
+  ROS_INFO("Reference frame for left: %s", groupLeft.getPlanningFrame().c_str());
+  ROS_INFO("Reference frame for left end effector: %s", groupLeft.getEndEffectorLink().c_str());
 
 
 
@@ -50,57 +61,90 @@ int main(int argc, char** argv){
      * Planning attempts should be one
      * Planning time depends on the gola location (7 should be enough)
      * Goal tolerance is required as we dont have a defined orientation
-     **********************************************************************/
-  group.setPlannerId("RRTkConfigDefault");
-  group.setNumPlanningAttempts(1);
-  group.setPlanningTime(7);
-  group.setGoalTolerance(0.1);
+  **********************************************************************/
+
+  groupRight.setPlannerId("RRTkConfigDefault");
+  groupRight.setNumPlanningAttempts(1);
+  groupRight.setPlanningTime(7);
+  groupRight.setGoalTolerance(0.1);
+
+  groupLeft.setPlannerId("RRTkConfigDefault");
+  groupLeft.setNumPlanningAttempts(1);
+  groupLeft.setPlanningTime(7);
+  groupLeft.setGoalTolerance(0.1);
 
   // set the target location
-  geometry_msgs::Pose t_pose_1;
-  geometry_msgs::Pose t_pose_2;
-  moveit::planning_interface::MoveGroup::Plan my_plan;
+  geometry_msgs::Pose tPoseRight;
+  geometry_msgs::Pose tPoseLeft;
+  moveit::planning_interface::MoveGroup::Plan myPlanRight, myPlanLeft;
 
   armTrajectory armTraj(node_handle);
   // set first pose point
   // The orientation constrains below are for the first task.
-  t_pose_1.orientation.w = 0.578;
-  t_pose_1.orientation.x = -0.255;
-  t_pose_1.orientation.y = -0.263;
-  t_pose_1.orientation.z = 0.729;
-  t_pose_1.position.x = 0.395;
-  t_pose_1.position.y = -0.291;
-  t_pose_1.position.z = 0.941;
-  group.setPoseTarget(t_pose_1,"rightShoulderPalm");
+  tPoseRight.orientation.w = 0.578;
+  tPoseRight.orientation.x = -0.255;
+  tPoseRight.orientation.y = -0.263;
+  tPoseRight.orientation.z = 0.729;
+  tPoseRight.position.x = 0.395;
+  tPoseRight.position.y = -0.291;
+  tPoseRight.position.z = 0.941;
+  groupRight.setPoseTarget(tPoseRight);
 
-  t_pose_2.orientation.w = 1.0;
-  t_pose_2.position.x = 0.394;
-  t_pose_2.position.y = 0.109;
-  t_pose_2.position.z = 0.943;
-  group.setPoseTarget(t_pose_2,"leftShoulderPalm");
-  group.setGoalTolerance(0.1);
 
-  if (group.plan((my_plan)))
+  tPoseLeft.orientation.w = -0.441;
+  tPoseLeft.position.x = 0.341;
+  tPoseLeft.position.y = 0.288;
+  tPoseLeft.position.z = 0.913;
+  tPoseLeft.orientation.x = -0.034;
+  tPoseLeft.orientation.y = -0.096;
+  tPoseLeft.orientation.z = 0.891;
+  groupLeft.setPoseTarget(tPoseLeft);
+
+
+  if (groupRight.plan((myPlanRight)))
   {
-        ROS_INFO("sucessfully planned the trajectory");
+        ROS_INFO("sucessfully planned the trajectory for right arm");
     std::vector<trajectory_msgs::JointTrajectoryPoint>::iterator itp;
 
         std::cout << "joint names" << std::endl;
-    for (it = my_plan.trajectory_.joint_trajectory.joint_names.begin(); it < my_plan.trajectory_.joint_trajectory.joint_names.end(); it++)
+    for (it = myPlanRight.trajectory_.joint_trajectory.joint_names.begin(); it < myPlanRight.trajectory_.joint_trajectory.joint_names.end(); it++)
       std::cout << *it << std::endl;
     std::cout << "joint trajectories" << std::endl;
-    for (itp = my_plan.trajectory_.joint_trajectory.points.begin(); itp < my_plan.trajectory_.joint_trajectory.points.end(); itp++)
+    for (itp = myPlanRight.trajectory_.joint_trajectory.points.begin(); itp < myPlanRight.trajectory_.joint_trajectory.points.end(); itp++)
       std::cout << *itp << std::endl;
 
     /* Sleep to give Rviz time to visualize the plan. */
     sleep(2.0);
     ROS_INFO("executing on robot\n");
 
-    armTraj.moveArmTrajectory(RIGHT, my_plan.trajectory_.joint_trajectory);
+    armTraj.moveArmTrajectory(RIGHT, myPlanRight.trajectory_.joint_trajectory);
   }
   else
   {
-    ROS_INFO("planning failed");
+    ROS_INFO("planning failed for Right Arm");
+  }
+
+  if (groupLeft.plan((myPlanLeft)))
+  {
+        ROS_INFO("sucessfully planned the trajectory for left arm");
+    std::vector<trajectory_msgs::JointTrajectoryPoint>::iterator itp;
+
+        std::cout << "joint names" << std::endl;
+    for (it = myPlanLeft.trajectory_.joint_trajectory.joint_names.begin(); it < myPlanLeft.trajectory_.joint_trajectory.joint_names.end(); it++)
+      std::cout << *it << std::endl;
+    std::cout << "joint trajectories" << std::endl;
+    for (itp = myPlanLeft.trajectory_.joint_trajectory.points.begin(); itp < myPlanLeft.trajectory_.joint_trajectory.points.end(); itp++)
+      std::cout << *itp << std::endl;
+
+    /* Sleep to give Rviz time to visualize the plan. */
+    sleep(2.0);
+    ROS_INFO("executing on robot\n");
+
+    armTraj.moveArmTrajectory(LEFT, myPlanLeft.trajectory_.joint_trajectory);
+  }
+  else
+  {
+    ROS_INFO("planning failed for left arm");
   }
 
 
