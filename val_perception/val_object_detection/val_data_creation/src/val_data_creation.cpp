ed62830b22d67eb2602982c30c919af824f7dcec
sahil <skejriwal@wpi.edu>
2017-04-12 18:49:28 -0400

    synchronisation added

--- val_perception/val_object_detection/val_data_creation/src/val_data_creation.cpp
+++ val_perception/val_object_detection/val_data_creation/src/val_data_creation.cpp
@@ -11,12 +11,16 @@
 #include <boost/bind.hpp>
 #include <tf/transform_listener.h>
 
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
 // too many header files ^
 
 int count = 0;
 cv::Mat left_image, right_image;
-bool left_updated, right_updated;
 tf::StampedTransform transform;
+tf::TransformListener listener;
+bool success = false;
 
 // /valve_frame is at (2.15 0.60 0 0 0 0 1 ) with respect to /world
 // This node is calculating the transform from /pelvis to /valve_frame
@@ -30,49 +34,6 @@ void writeToFile()
   filetext<<ros::package::getPath("val_data_creation")<<"/labels/"<<"Label_"<<count<<".txt";
   cv::imwrite(filenameL.str(),left_image);
   cv::imwrite(filenameR.str(),right_image);
-  std::ofstream fs;
-  fs.open(filetext.str().c_str());          // save transform to text file
-  fs << "xpos" << transform.getOrigin().x()<<"\n";
-  fs << "ypos" << transform.getOrigin().y()<<"\n";
-  fs << "zpos" << transform.getOrigin().z()<<"\n";
-  fs << "q1" << transform.getRotation()[1]<<"\n";
-  fs << "q2" << transform.getRotation()[2]<<"\n";
-  fs << "q3" << transform.getRotation()[3]<<"\n";
-  count++;
-  fs.close();
-  left_updated = right_updated = false;
-}
-
-void imageCallback(const sensor_msgs::ImageConstPtr& msg, char side)
-{
-  cv_bridge::CvImagePtr cv_ptr;
-  if (side == 'L')
-  {
-    left_updated = true;
-    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    left_image = cv_ptr->image; // convert msg to image
-  }
-  else if (side == 'R')
-  {
-    right_updated = true;
-    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    right_image = cv_ptr->image;
-  }
-
-  if (left_updated == true && right_updated == true)
-    { writeToFile(); }
-}
-
-
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "image_listener");
-  ros::NodeHandle nh;
-
-  bool success = false;
-  tf::TransformListener listener;
-
   while (!success) {
     try {
       listener.waitForTransform("/valve_frame", "/pelvis", ros::Time(0), ros::Duration(3.0));
@@ -84,10 +45,47 @@ int main(int argc, char **argv)
     }
     sleep(0.1); // not sure if required
   }
+  std::ofstream fs;
+  fs.open(filetext.str().c_str());          // save transform to text file
+  fs << "xpos " << transform.getOrigin().x()<<"\n";
+  fs << "ypos " << transform.getOrigin().y()<<"\n";
+  fs << "zpos " << transform.getOrigin().z()<<"\n";
+  fs << "q1 " << transform.getRotation()[1]<<"\n";
+  fs << "q2 " << transform.getRotation()[2]<<"\n";
+  fs << "q3 " << transform.getRotation()[3]<<"\n";
+  count++;
+  fs.close();
 
-  image_transport::ImageTransport it(nh);
-  image_transport::Subscriber subL = it.subscribe("/multisense/camera/left/image_raw", 1, boost::bind(imageCallback, _1, 'L'));
-  image_transport::Subscriber subR = it.subscribe("/multisense/camera/right/image_raw", 1, boost::bind(imageCallback, _1, 'R'));
+}
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msgL, const sensor_msgs::ImageConstPtr& msgR)
+{
+  cv_bridge::CvImagePtr cv_ptr;
+
+  cv_ptr = cv_bridge::toCvCopy(msgL, sensor_msgs::image_encodings::BGR8);
+  left_image = cv_ptr->image; // convert msg to image
+
+  cv_ptr = cv_bridge::toCvCopy(msgR, sensor_msgs::image_encodings::BGR8);
+  right_image = cv_ptr->image;
+
+  writeToFile();
+
+
+
+}
+
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "image_listener");
+  ros::NodeHandle nh;
+
+  message_filters::Subscriber<sensor_msgs::Image> subL(nh, "/multisense/camera/left/image_raw", 1); //boost::bind(imageCallback, _1, 'L')
+  message_filters::Subscriber<sensor_msgs::Image> subR(nh, "/multisense/camera/right/image_raw", 1);
+  message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image> sync(subL, subR, 10);
+  sync.registerCallback(boost::bind(&imageCallback, _1, _2));
+
   ros::spin();
 
 }
