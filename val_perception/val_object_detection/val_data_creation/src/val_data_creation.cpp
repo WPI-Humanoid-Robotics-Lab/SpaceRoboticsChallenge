cec9f13ddac73eb9db3e2bf83db20c2ea19596f7
sahil <skejriwal@wpi.edu>
2017-04-13 19:09:16 -0400

    Working version. synced images. transform updates

--- val_perception/val_object_detection/val_data_creation/src/val_data_creation.cpp
+++ val_perception/val_object_detection/val_data_creation/src/val_data_creation.cpp
@@ -1,64 +1,69 @@
 #include <ros/ros.h>
 #include <ros/package.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/highgui/highgui.hpp>
+
 #include <string>
-#include <opencv2/core/core.hpp>
-#include <iostream>
 #include <fstream>
 #include <boost/bind.hpp>
+
 #include <tf/transform_listener.h>
 
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui/highgui.hpp>
 
-// too many header files ^
+#include <image_transport/image_transport.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <image_transport/subscriber_filter.h>
+
 
 int count = 0;
 cv::Mat left_image, right_image;
+bool left_updated, right_updated;
 tf::StampedTransform transform;
-tf::TransformListener listener;
-bool success = false;
 
 // /valve_frame is at (2.15 0.60 0 0 0 0 1 ) with respect to /world
 // This node is calculating the transform from /pelvis to /valve_frame
 
-void writeToFile()
+void writeToFile() //std_msgs::Header h
 {
   // create file names
   std::stringstream filenameL, filenameR, filetext;
+
   filenameL<<ros::package::getPath("val_data_creation")<<"/left_images/"<<"L_"<<count<<".png";
   filenameR<<ros::package::getPath("val_data_creation")<<"/right_images/"<<"R_"<<count<<".png";
   filetext<<ros::package::getPath("val_data_creation")<<"/labels/"<<"Label_"<<count<<".txt";
+
   cv::imwrite(filenameL.str(),left_image);
   cv::imwrite(filenameR.str(),right_image);
+
+  bool success = false;
+  tf::TransformListener listener;
+
   while (!success) {
     try {
-      listener.waitForTransform("/valve_frame", "/pelvis", ros::Time(0), ros::Duration(3.0));
-      listener.lookupTransform("/valve_frame", "/pelvis", ros::Time(0), transform);
+      listener.waitForTransform("/valve_frame", "/pelvis", ros::Time(0), ros::Duration(2.0));
+      listener.lookupTransform("/valve_frame", "/pelvis", ros::Time(0), transform); //h.stamp to get image's timestamp
       success = true;
     }
-    catch (tf::LookupException e){
-
-    }
-    sleep(0.1); // not sure if required
+    catch (tf::LookupException e){}
   }
+
   std::ofstream fs;
   fs.open(filetext.str().c_str());          // save transform to text file
-  fs << "xpos " << transform.getOrigin().x()<<"\n";
-  fs << "ypos " << transform.getOrigin().y()<<"\n";
-  fs << "zpos " << transform.getOrigin().z()<<"\n";
-  fs << "q1 " << transform.getRotation()[1]<<"\n";
-  fs << "q2 " << transform.getRotation()[2]<<"\n";
-  fs << "q3 " << transform.getRotation()[3]<<"\n";
-  count++;
-  fs.close();
 
+  fs << "xpos" << transform.getOrigin().x()<<"\n";
+  fs << "ypos" << transform.getOrigin().y()<<"\n";
+  fs << "zpos" << transform.getOrigin().z()<<"\n";
+  fs << "q1" << transform.getRotation()[1]<<"\n";
+  fs << "q2" << transform.getRotation()[2]<<"\n";
+  fs << "q3" << transform.getRotation()[3]<<"\n";
+  count++;
+
+  fs.close();
 }
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msgL, const sensor_msgs::ImageConstPtr& msgR)
+void imageCallback(const sensor_msgs::ImageConstPtr& msgL, const sensor_msgs::ImageConstPtr& msgR )
 {
   cv_bridge::CvImagePtr cv_ptr;
 
@@ -68,24 +73,26 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msgL, const sensor_msgs::Im
   cv_ptr = cv_bridge::toCvCopy(msgR, sensor_msgs::image_encodings::BGR8);
   right_image = cv_ptr->image;
 
+  //std_msgs::Header h = msgR->header;
+
   writeToFile();
-
-
-
 }
-
-
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "image_listener");
   ros::NodeHandle nh;
 
-  message_filters::Subscriber<sensor_msgs::Image> subL(nh, "/multisense/camera/left/image_raw", 1); //boost::bind(imageCallback, _1, 'L')
-  message_filters::Subscriber<sensor_msgs::Image> subR(nh, "/multisense/camera/right/image_raw", 1);
-  message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image> sync(subL, subR, 10);
+  image_transport::ImageTransport it(nh);
+  image_transport::SubscriberFilter itfilterL, itfilterR;
+
+  itfilterL.subscribe(it,"/multisense/camera/left/image_raw", 1);
+  itfilterR.subscribe(it,"/multisense/camera/right/image_raw", 1);
+
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> mysync;
+
+  message_filters::Synchronizer<mysync> sync(mysync(10),itfilterL,itfilterR);
   sync.registerCallback(boost::bind(&imageCallback, _1, _2));
 
   ros::spin();
-
 }
